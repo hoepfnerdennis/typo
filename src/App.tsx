79de5048bc94abc578990b06cb5de20d0e41646a
hoepfnerdennis <dennishopfner@gmail.com>
2025-04-10 11:15:52 +0000

    neuer mode+ farben

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import pbj from "./assets/pbj.gif";
 import pbj2 from "./assets/pbj2.gif";
-import CHARACTERPOOL from "./characterpool";
+import CHARACTERPOOL, {hardcorepool} from "./characterpool";
 import useLocalState from "./useLocalState";
+
 
 type MessageType = {
   message: string;
@@ -10,22 +11,25 @@ type MessageType = {
 };
 
 function App() {
+  const [hardMode, setHardMode] = useState(false);
+  const [hardcoreMode, setHardcoreMode] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(hardcoreMode? 750:hardMode ? 1000 : 2000);
+  const [percentageLeft, setPercentageLeft] = useState(100);
   const [gameOver, setGameOver] = useState(true);
   const [checkChar, setCheckChar] = useState(
-    CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
+    hardcoreMode? hardcorepool[Math.floor(Math.random() * hardcorepool.length)]:CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
   );
   const [toast, setToast] = useState<MessageType | undefined>(undefined);
   const [score, setScore] = useLocalState("score", 0);
   const [highScore, setHighScore] = useLocalState("highScore", {
     normal: 0,
     hard: 0,
+    hardcore: 0,
   });
-  const [hardMode, setHardMode] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(hardMode ? 1000 : 2000);
-  const [percentageLeft, setPercentageLeft] = useState(100);
+
 
   useEffect(() => {
-    setPercentageLeft(timeLeft / (hardMode ? 10 : 20));
+    setPercentageLeft(timeLeft / (hardcoreMode? 7:hardMode ? 10 : 20));
   }, [timeLeft, percentageLeft]);
 
   // time-out toast
@@ -34,7 +38,7 @@ function App() {
       setScore(0);
       setToast({ message: "too slow", type: "error" });
       setCheckChar(
-        CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
+        hardcoreMode? hardcorepool[Math.floor(Math.random() * hardcorepool.length)] : CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
       );
       setGameOver(true);
     }
@@ -54,6 +58,10 @@ function App() {
               if (score + 1 > highScore.hard) {
                 setHighScore({ ...highScore, hard: highScore.hard + 1 });
               }
+            } else if (hardcoreMode) {
+              if (score + 1 > highScore.hardcore) {
+                setHighScore({ ...highScore, hardcore: highScore.hardcore + 1 });
+              }
             } else {
               if (score + 1 > highScore.normal) {
                 setHighScore({ ...highScore, normal: highScore.normal + 1 });
@@ -65,9 +73,9 @@ function App() {
             setGameOver(true);
           }
           setCheckChar(
-            CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
+            hardcoreMode? hardcorepool[Math.floor(Math.random() * hardcorepool.length)] : CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
           );
-          setTimeLeft(hardMode ? 1000 : 2000);
+          setTimeLeft(hardcoreMode? 750:hardMode ? 1000 : 2000);
         }
       }
     };
@@ -109,15 +117,16 @@ function App() {
   const handleClick = () => {
     setGameOver(false);
     setCheckChar(
-      CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
+      hardcorepool? hardcorepool[Math.floor(Math.random() * hardcorepool.length)] :CHARACTERPOOL[Math.floor(Math.random() * CHARACTERPOOL.length)]
     );
-    setTimeLeft(hardMode ? 1000 : 2000);
+    setTimeLeft(hardcoreMode? 500: hardMode ? 1000 : 2000);
   };
   const handleReset = () => {
     setScore(0);
     setHighScore({
       normal: 0,
       hard: 0,
+      hardcore: 0,
     });
   };
 
@@ -125,23 +134,27 @@ function App() {
     <div className="flex flex-col items-start justify-start h-screen text-xl font-bold gap-10">
       <div className="navbar bg-base-100 px-4">
         <div className="navbar-start">
-          <div className="flex gap-10 items-center">
+          <div className="flex gap-5 items-center">
             <button className="btn" onClick={handleReset}>
-              delete
+              reset highscore
             </button>
             <h2 className="hidden lg:flex">high score: </h2>
             <span className="text-sm">normal: {highScore.normal}</span>
-            <span className="text-sm text-error">hard: {highScore.hard}</span>
+            <span className="text-sm text-warning">hard: {highScore.hard}</span>
+            <span className="text-sm text-error">hardcore: {highScore.hardcore}</span>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
           {/* <a className="btn btn-ghost text-xl hidden md:flex">Typo 🍌</a> */}
           <h1
-            className={`w-full text-3xl text-center ${
-              hardMode && "text-error"
+            className={hardcoreMode?`w-full text-3xl text-center ${
+              hardcoreMode && "text-error"
+            }`:`w-full text-3xl text-center ${
+              hardMode && "text-warning"
             }`}
           >
-            {hardMode ? "HARDMODE 💀" : "normal mode"}
+
+            {hardcoreMode? "HARDCOREMODE☠️" : hardMode ? "HARDMODE 💀" : "normal mode"}
           </h1>
         </div>
         <div className="navbar-end gap-5">
@@ -149,11 +162,18 @@ function App() {
             <>
               <input
                 type="checkbox"
-                className="toggle"
+                className="toggle toggle-warning"
                 checked={hardMode}
                 onChange={() => setHardMode((prev) => !prev)}
               />
               💀
+              <input
+                type="checkbox"
+                className="toggle toggle-error"
+                checked={hardcoreMode}
+                onChange={() => setHardcoreMode((prev) => !prev)}
+              />
+              ☠️☠️☠️
             </>
           )}
         </div>
@@ -163,7 +183,7 @@ function App() {
         {gameOver ? (
           <div className="scale-[2.5] h-full flex items-center">
             <button
-              className={`btn ${hardMode && "btn-error"}`}
+              className={hardcoreMode?`btn ${hardcoreMode && "btn-error"}`:`btn ${hardMode && "btn-warning"}`}
               onClick={handleClick}
               autoFocus
             >
@@ -183,7 +203,7 @@ function App() {
               value={percentageLeft}
               max="100"
             ></progress>
-            <kbd className="kbd kbd-lg text-neutral-content scale-150 md:scale-[3] my-14">
+            <kbd className={`kbd kbd-lg text-neutral-content scale-150 md:scale-[3] my-14 ${hardcoreMode? "bg-error": hardMode?"bg-warning": ""}`}>
               {checkChar}
             </kbd>
             <span>score: {score}</span>
